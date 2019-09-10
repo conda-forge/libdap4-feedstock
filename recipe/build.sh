39f153c5181f53a1b08a2ce1f7a29bf768596946
Filipe Fernandes <ocefpaf@gmail.com>
2019-09-10 10:41:43 -0300

    unpin bison

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Yes, this is actually meant to be CPPFLAGS (preprocessor flags)
-export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 # Set c++ std away from default of c++17 due to https://www.mail-archive.com/gcc@gcc.gnu.org/msg81929.html
 # error text is "error: ISO C++1z does not allow dynamic exception specifications"
-export CXXFLAGS="-I$PREFIX/include $CXXFLAGS -std=c++14"
+export CXXFLAGS="-I${PREFIX}/include ${CXXFLAGS} -std=c++14"
 
 autoreconf --force --install
 
-bash configure --prefix=$PREFIX \
-               --with-xml2=$PREFIX \
+bash configure --prefix=${PREFIX} \
+               --with-xml2=${PREFIX} \
                --with-curl=${PREFIX} \
                --enable-threads=pth
 
